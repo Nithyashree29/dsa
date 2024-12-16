92ba381f938203c4493c9d704ec5719123995c90
Nithyashree S <nithyashree.s@in.ey.com>
2024-12-16 15:08:33 +0530

    hash

--- first_recurring.py
+++ first_recurring.py
@@ -7,3 +7,21 @@
 # Given Array = [2,3,4,5]
 # should return undefined
                
+
+def recurring_num(arr):
+    if not arr:
+        return "Undefined"
+
+    count_dict = {}
+    for i in arr:
+        if i in count_dict:
+            count_dict[i] += 1
+        else:
+            count_dict[i] = 1
+    max_val = max(count_dict.values())
+    # print(max_val)
+    return {k:v for k,v in count_dict.items() if v == max_val}
+
+
+print(recurring_num([2,3,5,7,3,2,45,7,2,7]))
+
