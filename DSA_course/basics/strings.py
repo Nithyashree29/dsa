02fb4ffdf756bfad73ce3caeb675c3e467c351a8
Your Name <you@example.com>
2025-12-25 10:10:38 +0530

    latest code

--- DSA_course/basics/strings.py
+++ DSA_course/basics/strings.py
@@ -15,15 +15,15 @@ def reverse1(val):
     for i in range(n):
         dup[n-i-1] = val[i]
     return ' '.join(dup)
-
-def opt_reverseString(text):
-    char = s.strip().split()
+ 
+def opt_reverseString(char):
+    char = char.strip().split()
     left, right = 0, len(char)-1
     while left < right:
         char[left], char[right] = char[right], char[left]
         left +=1
         right -= 1
-    return ''.join(text)       
+    return ''.join(char)       
                     
             
 def reverse_2(text):
@@ -42,4 +42,35 @@ def palindrome(s):
         right -= 1
     return True
 
-                                                                                                                                                                                     
+#Given s atring s, representing a large integer, the task is to return the largest-values odd integer(as a string) that is the substring of the given string s . the number returned should not have leading sero's. Bute the given input string may have leading sero.
+#Using sub-string is a better option 
+def largestOddNumber(s):
+    ind = -1
+    i = 0
+    for i in range(len(s)-1, -1, -1):
+        if (int(s[i]) % 2) == 1:
+            ind = i
+            break
+        
+    i = 0
+    while i < ind and s[i] == '0':
+        i += 1
+    return s[i:ind+1]  #TC - O(N), SC - O(1)
+
+
+def longestCommonPrefix(arr):  #  sort it for better, and compare first and last
+    if not arr:
+        return ""
+    arr.sort()
+    first = arr[0]
+    last = arr[-1]
+    ans = []
+    for i in range(min(len(first), len(last))):
+        if first[i] != last[i]:
+            return ''.join(ans)
+        ans.append(first[i])
+    return ''.join(ans)
+
+
+       
+                                                                                                                                                                              
